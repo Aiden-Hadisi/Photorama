db25ba791e917bca9f4d94bfa1b2676a93af418d
Aiden Hadisi <so.hadisi@gmail.com>
2020-05-10 20:44:00 -0700

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by CSUFTitan on 5/10/20.
+//  Copyright © 2020 Aiden Hadisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
